ad46c9266869ab0a191b6ce628a1051c3eb0c6d3
colesmith <colesmith@ls-imac.lan>
2021-08-20 18:53:04 -0400

    evgen.java
    Introduce pizero decay

--- src/main/java/org/clas/tools/evgen.java
+++ src/main/java/org/clas/tools/evgen.java
@@ -8,10 +8,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.jlab.clas.physics.Particle;
+import org.jlab.clas.physics.ParticleGenerator;
+import org.jlab.clas.physics.PhysicsEvent;
 
 public class evgen {
-	
-	Particle p = new Particle();
 	
 	List<String> out = new ArrayList<String>();
 	int   nc;
@@ -23,8 +23,11 @@ public class evgen {
 	}
 	
 	public void genEvents(String str, int val) {
-		if(str=="pim_g_g") ge = new gen_pim_g_g();
+		if(str=="pim_pi0") ge = new gen_pim_pi0();
+		if(str=="pim_gam") ge = new gen_pim_gam();
 		if(str=="e_pim")   ge = new gen_e_pim();
+		if(str=="e_mum")   ge = new gen_e_mum();
+		if(str=="e_all")   ge = new gen_e_all();
 		writeFile(str,val);
 	}
 		
@@ -43,12 +46,67 @@ public class evgen {
 		}		
 	}
 	
+	public class gen_pim_gam extends GenEvent {		
+		public List<String> getev() {
+			out.clear();
+			out.add(getHeader(2,nc));
+			float vtx = (float) (-5.5+Math.random()*5);
+			out.add(getString(getParticle(-211,2,2,18,18,240,260,vtx),1));
+			out.add(getString(getParticle(  22,0.1,4,5.5,25.5,45,75,vtx),2));
+			return out;			
+		}		
+	}	
+	
+	public class gen_pim_pi0 extends GenEvent {		
+		public List<String> getev() {
+			out.clear();
+			List<Particle> plist = getDecay(111,4,8,14,16,-5,5);
+			out.add(getHeader(3,nc));
+			out.add(getString(getParticle(-211,2,2,18,18,240,260),1));
+			out.add(getString(plist.get(0),2));
+			out.add(getString(plist.get(1),3));
+			return out;			
+		}		
+	}
+	
 	public class gen_e_pim extends GenEvent {		
 		public List<String> getev() {
 			out.clear();
 			out.add(getHeader(2,nc));
 			out.add(getString(getParticle(  11,7,9,10,25,240,260),1));
-			out.add(getString(getParticle(-211,2.0,5,15,30, 59, 61),2));
+			out.add(getString(getParticle(-211,2,5,15,30, 59, 61),2));
+			return out;			
+		}		
+	}
+	
+	public class gen_e_pip extends GenEvent {		
+		public List<String> getev() {
+			out.clear();
+			out.add(getHeader(2,nc));
+			out.add(getString(getParticle(  11,7,9,10,25,240,260),1));
+			out.add(getString(getParticle(+211,2,5,15,30, 59, 61),2));
+			return out;			
+		}		
+	}
+	
+	public class gen_e_all extends GenEvent {		
+		public List<String> getev() {
+			out.clear();
+			out.add(getHeader(4,nc));
+			out.add(getString(getParticle(   11,0.80,2.0, 5,25,250,270),1));
+			out.add(getString(getParticle(+2212,0.55,1.4,28,30, 12, 52),2));
+			out.add(getString(getParticle( +211,0.40,1.4,28,30, 72,112),3));
+			out.add(getString(getParticle( -211,0.40,1.4,28,30,132,172),4));
+			return out;			
+		}		
+	}
+	
+	public class gen_e_mum extends GenEvent {		
+		public List<String> getev() {
+			out.clear();
+			out.add(getHeader(2,nc));
+			out.add(getString(getParticle(  11,7,9,10,25,240,260),1));
+			out.add(getString(getParticle(  13,2,5,15,30, 59, 61),2));
 			return out;			
 		}		
 	}
@@ -58,6 +116,7 @@ public class evgen {
 	}
 	
 	public Particle getParticle(double... pargs) {
+		Particle p = new Particle();
 		int   pid = (int) pargs[0];
 		double pm = pargs[1]+Math.random()*(pargs[2]-pargs[1]);
 	    double th = Math.toRadians((pargs[3]+Math.random()*(pargs[4]-pargs[3])));
@@ -65,8 +124,21 @@ public class evgen {
 	    double px = pm*Math.sin(th)*Math.cos(ph);
 	    double py = pm*Math.sin(th)*Math.sin(ph);
 	    double pz = pm*Math.cos(th);		
-	    p.setVector(pid,px,py,pz,0,0,0);
+	    p.setVector(pid,px,py,pz,0,0,pargs.length>7?pargs[7]:0);
 	    return p;
+	}
+	
+	public List<Particle> getDecay(double... pargs) {		
+		List<Particle> plist = new ArrayList<Particle>();		
+		TwoBodyDecay decay = new TwoBodyDecay();
+    	ParticleGenerator pg = new ParticleGenerator((int)pargs[0]);    	
+    	pg.setRange(pargs[1],pargs[2],pargs[3],pargs[4],pargs[5],pargs[6]); 
+    	PhysicsEvent event = new PhysicsEvent(); event.clear();
+    	event.addParticle(pg.getParticle());
+    	decay.decayParticles(event);
+    	plist.add(event.getGeneratedParticle(0)); 
+    	plist.add(event.getGeneratedParticle(1));    	
+		return plist;
 	}
 	
 	public String getString(Particle p, int np) {
@@ -85,7 +157,7 @@ public class evgen {
 		List<String> list = new ArrayList<String>();
 		
 		try { 
-			File outputFile = new File("/Users/colesmith/CLAS12/sim/2gamma/"+str+".lund");
+			File outputFile = new File("/Users/colesmith/CLAS12/sim/photon/"+str+".lund");
 			FileWriter outputFw = new FileWriter(outputFile.getAbsoluteFile());
 			BufferedWriter outputBw = new BufferedWriter(outputFw);
 			
@@ -115,8 +187,12 @@ public class evgen {
 	
 	public static void main(String[] args) {
 		evgen gen = new evgen();
-		gen.genEvents("e_pim",10000);
+//		gen.genEvents("e_pim",10000);
+//		gen.genEvents("e_mum", 10000);
+//		gen.genEvents("e_all", 50000);
 //		gen.genEvents("pim_g_g",1000);
+//		gen.genEvents("pim_pi0",10000);
+		gen.genEvents("pim_gam",50000);
 	}
 
 }
